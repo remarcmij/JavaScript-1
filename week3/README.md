e99ec75fa07f9023f8e9dcffbbe3eb2ee5a325b8
Jim Cramer <remarcmij@gmail.com>
2017-11-10 23:09:29 +0100

    add homework correction to week 3

--- week3/README.md
+++ week3/README.md
@@ -2,20 +2,29 @@
 
 > Don't forget to start a recording!
 
+## Homework
+
+- Correct and refactor the week 1 homework of one student
+
 ## Scope
 
 - Use dictionary.com to look up what the word 'scope' means.
 - Scope: what you can see (from where you are)
 - Explain scope by creating an `index.html`, `app.js` and `style.css` triplet
-- Explain that global scope is a crowded place
+- Show and explain that global scope is a crowded place (use debugger in browser)
 - Take class 12 to be the global scope and create a new variable `mohammed`: it will clash
+- Show var, let, const effects on scope
+- strict mode
+- function scope
+- block scope
+- iffe
 
 ## State
 
 - Explain state with chess board analogy.
-- Use roverjs to explain constant and variable state
+- Use roverjs introduction level to discuss state
 
 
-## Array manipulation
+## JavaScript in the BROWSER with HTML and CSS
 
-- Show devdocs
+- Progressively build the rover introduction level from ASCII art to SPA using live coding (see final folder)
---
+++ week3/live/homework/abdullah.js
@@ -0,0 +1,104 @@
+
+// Q1
+console.log('Hello World');
+console.log("مرحبا العالم ");
+console.log("Hallo Wereld");
+
+// Q2
+console.log('I\'m awesome');
+
+//Q3
+var x = 7;
+console.log(x);
+console.log('the value of x will be: 7');
+
+//Q4
+ var y = "Youtube";
+ console.log('the value of y will be: Youtube');
+ console.log(y);
+ y = "Yoga";
+ console.log('the value of y will be: Yoga');
+ console.log(y);
+
+ //Q5
+  var z = 7.25;
+  console.log(z);
+  var a = 0.75;
+  console.log(a);
+  var sum = z + a;
+  console.log(sum);
+
+//Q6
+ var emptyArray = [];
+ console.log("It'll give me only brackets without values");
+ console.log(emptyArray);
+ var myFavoriteAnimals = ['cat', 'dog', 'bunny', 'lion'];
+ console.log(myFavoriteAnimals);
+ myFavoriteAnimals = ['cat', 'dog', 'bunny', 'lion', 'Daans' ];
+ console.log(myFavoriteAnimals);
+
+ //Q7
+ let myString = "this,is,a,test";
+ console.log(myString);
+ var theLength = myString.length;
+ console.log(theLength);
+
+ //Q8
+ var theFirst = 10;
+ var theSecond = "Hello";
+ var theThird = 25;
+ var theFourth = "Google";
+
+ console.log('The value of my variable theFirst is: ' + theFirst);
+ console.log('The value of my variable theSecond is: ' + theSecond);
+ console.log('The value of my variable theThird is: ' + theThird);
+ console.log('The value of my variable theFourth is: ' + theFourth);
+
+ console.log("The value's of my variables are string and integer ");
+
+ console.log(typeof theFirst);
+ console.log(typeof theSecond);
+ console.log(typeof theThird);
+ console.log(typeof theFourth);
+
+// if statement that return TRUE because the variables have the same type.
+ if (typeof theFirst === typeof theThird){
+    console.log("SAME TYPE");
+ }
+ else{
+     console.log("DIFFERENT TYPE");
+ }
+
+//  if statement that return FALSE because the variables doesn't have the same type.
+ if (typeof theFirst === typeof theSecond){
+    console.log("SAME TYPE");
+    }
+     else{
+         console.log("DIFFERENT TYPE");
+     }
+
+//Q9
+
+var num = 7;
+var percentSign = num % 3;
+console.log(percentSign);
+console.log("This sign means that the numbers when you divide them by another number the rest would be 1 or more");
+console.log("For instance if we change 3 with 5 the then the rest would be 2");
+
+//Q10
+
+console.log("Q1 >> yes we can store numbers and strings in a array")
+
+if (6/0 === 10/0){
+
+  console.log("True");
+}
+else{
+    console.log("False");
+}
+
+console.log("we will get her True because both number don't have rest, the rest is 0 ");
+
+
+
+
