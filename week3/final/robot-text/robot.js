37538b92894b44f003ba678c6fdf6c97fc602a0f
Elmira Beyti <33642240+elmira202003@users.noreply.github.com>
2018-01-13 14:10:44 +0100

    Add files via upload

--- week3/final/robot-text/robot.js
+++ week3/final/robot-text/robot.js
@@ -1,110 +1,123 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  const board = [
-    ['T', 'T', '.', 'F'],
-    ['T', '.', '.', '.'],
-    ['.', '.', '.', '.'],
-    ['R', '.', '.', 'W']
-  ];
+    const board = [
+        ['.', '.', '.', '.','.'],
+        ['.', '.', '.', '.','.'],
+        ['R', '.', '.', '.','F'],
+        ['.', 'A', 'A', 'A','.'],
+        ['.', '.', '.', '.','.']
+    ];
 
-  const robot = {
-    x: 0,
-    y: 0,
-    dir: 'up',
-  };
+    const robot = {
+        x: 0,
+        y: 2,
+        dir: 'right'
+    };
 
-  let flagReached = false;
-  let moves = 0;
+    let flagReached = false;
+    let appleeaten = false;
+    let appleseaten = 0;
+    let moves = 0;
 
-  board.reverse();
+    board.reverse();
 
-  const trailIndicators = {
-    left: '←',
-    right: '→',
-    up: '↑',
-    down: '↓'
-  };
+    const trailIndicators = {
+        left: '←',
+        right: '→',
+        up: '↑',
+        down: '↓'
+    };
 
-  function render() {
-    console.log('\n ' + moves + ':');
-    for (let row = board.length - 1; row >= 0; row--) {
-      const cells = board[row];
-      let line = '';
-      for (let col = 0; col < cells.length; col++) {
-        line += ' ' + cells[col] + ' ';
-      }
-      console.log(line);
-    }
-    if (flagReached) {
-      console.log('\nHurray! Flag reached in ' + moves + ' steps!');
-    }
-  }
-
-  function move() {
-    let x = robot.x;
-    let y = robot.y;
-
-    switch (robot.dir) {
-      case 'up':
-        y = y < board.length - 1 ? y + 1 : y;
-        break;
-      case 'down':
-        y = y > 0 ? y - 1 : y;
-        break;
-      case 'left':
-        x = x > 0 ? x - 1 : x;
-        break;
-      case 'right':
-        x = x < board[y].length - 1 ? x + 1 : x;
-        break;
+    function render() {
+        console.log('\n ' + moves + ':');
+        for (let row = board.length - 1; row >= 0; row--) {
+            const cells = board[row];
+            let line = '';
+            for (let col = 0; col < cells.length; col++) {
+                line += ' ' + cells[col] + ' ';
+            }
+            console.log(line);
+        }
+        if (appleeaten) { console.log('\nYUM!');}
+        if (flagReached) {
+            console.log('\nHurray! Flag reached in ' + moves + ' steps!');
+            console.log('\n'+ appleseaten + 'apples are eaten!');
+        }
     }
 
-    const cellContents = board[y][x];
+    function move() {
+        let x = robot.x;
+        let y = robot.y;
+        appleeaten = false;
 
-    if (cellContents === '.' || cellContents === 'F') {
-      board[robot.y][robot.x] = trailIndicators[robot.dir];
-      robot.x = x;
-      robot.y = y;
-      board[y][x] = 'R';
-      if (cellContents === 'F') {
-        flagReached = true;
-      }
+
+        switch (robot.dir) {
+            case 'up':
+                y = y < board.length - 1 ? y + 1 : y;
+                break;
+            case 'down':
+                y = y > 0 ? y - 1 : y;
+                break;
+            case 'left':
+                x = x > 0 ? x - 1 : x;
+                break;
+            case 'right':
+                x = x < board[y].length - 1 ? x + 1 : x;
+                break;
+        }
+
+        const cellContents = board[y][x];
+
+        if (cellContents === '.' || cellContents === 'F' || cellContents === 'A') {
+            board[robot.y][robot.x] = trailIndicators[robot.dir];
+            robot.x = x;
+            robot.y = y;
+            board[y][x] = 'R';
+            if (cellContents === 'F') {
+                flagReached = true;
+                
+            }
+            if (cellContents === 'A') {
+                appleseaten += 1;
+            }
+            if (appleseaten == 3) { appleeaten = true; }
+        }
+
+       moves += 1;
+        render();
     }
 
-    moves += 1;
+    function turn(turnDirection) {
+        if (turnDirection !== 'left' && turnDirection !== 'right') {
+            console.log('ignoring invalid turn', turnDirection);
+        }
+        switch (robot.dir) {
+            case 'up':
+                robot.dir = turnDirection === 'left' ? 'left' : 'right';
+                break;
+            case 'down':
+                robot.dir = turnDirection === 'left' ? 'right' : 'left';
+                break;
+            case 'left':
+                robot.dir = turnDirection === 'left' ? 'down' : 'up';
+                break;
+            case 'right':
+                robot.dir = turnDirection === 'left' ? 'up' : 'down';
+                break;
+        }
+    }
+
     render();
-  }
 
-  function turn(turnDirection) {
-    if (turnDirection !== 'left' && turnDirection !== 'right') {
-      console.log('ignoring invalid turn', turnDirection);
-    }
-    switch (robot.dir) {
-      case 'up':
-        robot.dir = turnDirection === 'left' ? 'left' : 'right';
-        break;
-      case 'down':
-        robot.dir = turnDirection === 'left' ? 'right' : 'left';
-        break;
-      case 'left':
-        robot.dir = turnDirection === 'left' ? 'down' : 'up';
-        break;
-      case 'right':
-        robot.dir = turnDirection === 'left' ? 'up' : 'down';
-        break;
-    }
-  }
-
-  render();
-
-  move();
-  turn('right');
-  move();
-  move();
-  move();
-  turn('left');
-  move();
-  move();
+    move();
+    turn('right');
+    move();
+    turn('left');
+    move();
+    move();
+    move();
+    turn('left');
+    move();
 
 })();
